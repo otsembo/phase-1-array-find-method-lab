8b16542b101f3cf4398834716df6ac02cd04d7c5
otsembo <okumu.otsembo@gmail.com>
2022-03-01 15:07:59 +0300

    commit tree 224cffe3976bf3a274de163cca5719c9545e5b7e

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record){
+    let year;
+    for (let i = 0; i<record.length; i++){
+        const data = record[i];
+        if(data.result == "W"){
+            year = data.year;
+            break;
+        }
+    }
+    return year;
+}
